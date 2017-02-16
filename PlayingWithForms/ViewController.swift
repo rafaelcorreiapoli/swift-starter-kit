40b5613e8fbe230b596521403111fff62e18e37b
Rafael Ribeiro Correia <rafael.correia.poli@gmail.com>
2017-02-16 15:10:08 -0200

    Animated tab controller

--- PlayingWithForms/ViewController.swift
+++ PlayingWithForms/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class ViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        form +++ Section()
+            <<< TextRow(){ row in
+                row.title = "Job"
+                row.tag = "job"
+            }
+            <<< TextRow(){ row in
+                row.title = "Name"
+                row.tag = "name"
+            }
+            <<< DateRow(){ row in
+                row.title = "Birthdate"
+                row.value = Date()
+                row.tag = "birthday"
+                }
+            <<< ButtonRow() { row in
+                row.title = "Send"
+                row.tag = "button"
+                row.onCellSelection({ (_:ButtonCellOf<String>, _: (ButtonRow)) in
+                    // Get the value of a single row
+                    let row: TextRow? = self.form.rowBy(tag: "name")
+                    if let value = row?.value {
+                        print(value);
+                    }
+                    
+                    // Get the value of all rows which have a Tag assigned
+                    // The dictionary contains the 'rowTag':value pairs.
+                    let valuesDictionary = self.form.values()
+                    print(valuesDictionary);
+                    if let birthday = valuesDictionary["birthday"] {
+                        print("aniversario: \(birthday)")
+                    }
+                })
+            }
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Pods/Target Support Files/Pods-PlayingWithForms/Pods-PlayingWithForms-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlayingWithForms : NSObject
+@end
+@implementation PodsDummy_Pods_PlayingWithForms
+@end
